d376f713727812ed246c81aed12716ee95733bbb
billfedriks <76469626+febriliansetiawan@users.noreply.github.com>
2023-03-20 21:18:08 +0700

    Progress 1 ppt

--- app/src/main/java/com/example/anmpweek3kpc/GameFragment.kt
+++ app/src/main/java/com/example/anmpweek3kpc/GameFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_game.*
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class GameFragment : Fragment() {
 
@@ -14,6 +17,14 @@ class GameFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnBack.setOnClickListener {
+            val action = GameFragmentDirections.actionMainFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 
 }
